b5c6f049c70bf84fed1410b0ecd9bc9dc4cdc4f8
zhu-liqun <2263413719@qq.com>
2026-06-24 16:12:54 +0800

    主页tab栏

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,21 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/Mine/index.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage({Key? key}) : super(key: key);
+  const MainPage({super.key});
 
   @override
   _MainPageState createState() => _MainPageState();
 }
 
 class _MainPageState extends State<MainPage> {
+  //定义数据，根据数据渲染四个导航
+  //一般程序的导航栏是固定的
+  final List<Map<String, String>> _tabList = [
+    {'icon': 'lib/assets/ic_public_home_normal.png', 'active_icon': 'lib/assets/ic_public_home_active.png', 'title': '首页'},
+    {'icon': 'lib/assets/ic_public_pro_normal.png', 'active_icon': 'lib/assets/ic_public_pro_active.png', 'title': '分类'},
+    {'icon': 'lib/assets/ic_public_cart_normal.png', 'active_icon': 'lib/assets/ic_public_cart_active.png', 'title': '购物车'},
+    {'icon': 'lib/assets/ic_public_my_normal.png', 'active_icon': 'lib/assets/ic_public_my_active.png', 'title': '我的'},
+  ];
+  //根据数据渲染导航栏
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(_tabList.length, (index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tabList[index]['icon']!, width: 30, height: 30),//!!表示非空断言，确保图标路径不为空
+        activeIcon: Image.asset(_tabList[index]['active_icon']!, width: 30, height: 30),//!!表示非空断言，确保图标路径不为空
+        label: _tabList[index]['title']!,//!!表示非空断言，确保标题不为空
+      );
+    });
+  }
+  int _currentIndex = 0;
+  List<Widget> _getChildren(){
+    return [HomeView(),CategoryView(),CartView(),MineView()];
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-        title: Text('主页'),
-       ),
-       body: Center(
-        child: Text('主页'),
+      //避开安全区
+       body: SafeArea(child: IndexedStack(
+        index: _currentIndex,//当前选中的索引
+        children: _getChildren(),//放置几个组件
+       )),
+       bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true,//显示选中的文本
+        unselectedItemColor: Colors.black,//未选中的文本颜色
+        selectedItemColor: Colors.black,//选中的文本颜色
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          //index表示当前点击的索引
+          setState(() {
+            _currentIndex = index;
+          });//通过setState方法更新状态，触发重新构建界面
+        },
+        items: _getTabBarWidget(),
        ),
     );
   }
