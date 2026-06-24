6af2bdf14110375ed8923a14d5ba38e58491ded8
zhu-liqun <2263413719@qq.com>
2026-06-24 15:23:43 +0800

    搭建基础路由和组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+//返回App跟级组件
+Widget getRootWidget() {
+  return MaterialApp(
+    //命名路由
+    initialRoute: '/',
+    routes: getRootRoutes(),
+  );
+}
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {
+    '/': (context) => MainPage(),//主页路由
+    '/login': (context) => LoginPage(),//登录页路由
+  };
+}
